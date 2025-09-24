6091b8996a2e20e17abe40bb94b48f72ae64ee38
TheGiat-tech <225952250+TheGiat-tech@users.noreply.github.com>
2025-09-25 00:47:53 +0300

    Add Google Analytics scripts to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 
@@ -12,6 +13,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LD6QWT7SJ0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-LD6QWT7SJ0');
+    `}
+        </Script>
+      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
